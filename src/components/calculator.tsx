9758269205f882a5ad4e4c5c240cd66099090f4e
ekim611 <ekim611@gatech.edu>
2025-04-05 02:39:19 -0400

    added skeleton ui for calculator

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,4 +1,5 @@
 'use client' 
+import { Button } from "@/components/ui/button"
 
 export function Calculator() { 
 
@@ -11,6 +12,39 @@ export function Calculator() {
       <div className="bg-white dark:bg-slate-700 p-3 mb-4 rounded-md text-right text-2xl h-16 flex items-center justify-end border">
         0
       </div>
+      
+      <div className="grid grid-cols-4 gap-2.5"> 
+        {/* First Row */}
+        <Button variant="secondary" className="border">(</Button>
+        <Button variant="secondary" className="border">)</Button>
+        <Button variant="secondary" className="border">%</Button>
+        <Button variant="destructive" className="border">AC</Button>
+
+        {/* Second Row */}
+        <Button variant="secondary" className="border">7</Button>
+        <Button variant="secondary" className="border">8</Button>
+        <Button variant="secondary" className="border">9</Button>
+        <Button variant="secondary" className="border">÷</Button>
+
+        {/* Third Row */}
+        <Button variant="secondary" className="border">4</Button>
+        <Button variant="secondary" className="border">5</Button>
+        <Button variant="secondary" className="border">6</Button>
+        <Button variant="secondary" className="border">×</Button>
+
+        {/* Fourth Row */}
+        <Button variant="secondary" className="border">1</Button>
+        <Button variant="secondary" className="border">2</Button>
+        <Button variant="secondary" className="border">3</Button>
+        <Button variant="secondary" className="border">-</Button>
+
+        {/* Fifth Row */}
+        <Button variant="secondary" className="border">0</Button>
+        <Button variant="secondary" className="border">.</Button>
+        <Button variant="secondary" className="border">=</Button>
+        <Button variant="secondary" className="border">+</Button>
+
+      </div> 
     </div> 
 
   )
