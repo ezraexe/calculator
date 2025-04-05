1240dbb51754d1e307cb2fe91ae42a55cbf494dd
ekim611 <ekim611@gatech.edu>
2025-04-05 02:20:46 -0400

    added container and display

---
+++ src/components/calculator.tsx
@@ -0,0 +1,17 @@
+'use client' 
+
+export function Calculator() { 
+
+
+
+
+
+  return (
+    <div className="w-96 border rounded-xl bg-muted shadow-xl p-4"> 
+      <div className="bg-white dark:bg-slate-700 p-3 mb-4 rounded-md text-right text-2xl h-16 flex items-center justify-end border">
+        0
+      </div>
+    </div> 
+
+  )
+}
